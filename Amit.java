f3ac46fe64fa63dc962ea511757dc7813a5c10dd
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-06-01 22:15:08 +0530

    loop

--- Amit.java
+++ Amit.java
@@ -1,36 +1,15 @@
 
-import java.util.Scanner;
-
 public class Amit {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:-");
-        int number = sc.nextInt();
-        switch (number) {
-            case 1:
-                System.out.println("january");
-                break;
-            case 2:
-                System.out.println("fabuary");
-                break;
-            case 3:
-                System.out.println("march");
-                break;
-            case 4:
-                System.out.println("april");
-                break;
-            case 5:
-                System.out.println("may");
-                break;
-            case 6:
-                System.out.println("june");
-                break;
-            case 7:
-                System.out.println("july");
-                break;
-            default:
-                System.out.println("invalid number");
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
         }
+
     }
 }
