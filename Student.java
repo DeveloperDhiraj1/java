33d5829c6e669643e26055b13d44e11452da28a5
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-06-16 20:42:54 +0530

    inheritance

--- Student.java
+++ Student.java
@@ -3,15 +3,18 @@ class Student {
 
     String name;
     int age;
+    int roll_no;
 
     Student(String studentName, int studentAge) {
         name = studentName;
         age = studentAge;
+        roll_no = 9012;
     }
 
     void display() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("roll_no" + roll_no);
     }
 
     public static void main(String[] args) {
