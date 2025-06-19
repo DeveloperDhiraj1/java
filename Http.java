b9747247557f91f31705acbd047791e6f4797bba
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-06-19 21:31:13 +0530

    ...

--- Http.java
+++ Http.java
@@ -7,22 +7,17 @@ public class Http {
     public static void sendGet() throws IOException {
         URL url = new URL("https://httpbin.org/get");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
         con.setRequestMethod("GET");
-
         int responseCode = con.getResponseCode();
         System.out.println("GET Response Code :: " + responseCode);
-
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuilder response = new StringBuilder();
-
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-
             System.out.println("GET Response: " + response.toString());
         } else {
             System.out.println("GET request failed.");
@@ -32,26 +27,20 @@ public class Http {
     public static void sendPost() throws IOException {
         URL url = new URL("https://httpbin.org/post");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
         con.setRequestMethod("POST");
         con.setDoOutput(true);
         con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
         String postData = "name=Dhiraj&age=20";
-
         try (OutputStream os = con.getOutputStream()) {
             byte[] input = postData.getBytes("utf-8");
             os.write(input, 0, input.length);
         }
-
         int responseCode = con.getResponseCode();
         System.out.println("POST Response Code :: " + responseCode);
-
         if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_CREATED) {
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
             String inputLine;
             StringBuilder response = new StringBuilder();
-
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine.trim());
             }
