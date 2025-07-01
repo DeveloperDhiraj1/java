bbf4ca1e3a33159a776d0340c342b0e09c1b13d9
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-07-01 23:05:38 +0530

    list

--- List.java
+++ List.java
@@ -8,11 +8,14 @@ public class List {
         l.add(1);
         l.add(2);
         l.add(3);
+        l.add(4);
         System.out.println(l);
         System.out.println(l.get(1));
         l.set(0, 10);
+        l.set(1, 20);
+        l.set(2, 30);
+        l.set(3, 40);
         System.out.println(l);
-        System.out.println(l.contains(10));
     }
 
     public static void main(String[] args) {
