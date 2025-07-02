24c6671b7ef74226388eddd00eb5a77b37ae3947
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-07-02 21:30:24 +0530

    linkedlist

--- Linked.java
+++ Linked.java
@@ -8,10 +8,12 @@ public class Linked {
         l.add(1);
         l.add(2);
         l.add(3);
+        l.add(4);
         System.out.println(l);
         l.set(0, 20);
         l.set(1, 90);
         l.set(2, 111);
+        l.set(3, 231);
         System.out.print(l);
     }
 
