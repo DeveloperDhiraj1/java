8ffdadcc94a359fea981e8d2970f5ada99518841
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-07-03 23:23:23 +0530

    ll

--- Linked.java
+++ Linked.java
@@ -9,11 +9,13 @@ public class Linked {
         l.add(2);
         l.add(3);
         l.add(4);
+        l.add(5);
         System.out.println(l);
         l.set(0, 20);
         l.set(1, 90);
         l.set(2, 111);
         l.set(3, 231);
+        l.set(4, 32);
         System.out.print(l);
     }
 
